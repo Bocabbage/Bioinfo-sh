d9b56954fc51b0d256959d7a3f7a9761bc5c1bcd
Bocabbage <378066505@qq.com>
2019-05-14 17:11:11 +0800

    Start RNA-SEQ.

---
+++ rna_seq/download_sra.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SRA_LST=(SRR2920574 SRR2920572 SRR2753137 SRR2753131 SRR2753135 SRR2753130)
+OUTPATH="/home/student2/zzf/sra"
+TOOLPATH="/home/lihm/software/sratoolkit.2.8.2-1-ubuntu64/bin"
+
+for sra in "${SRA_LST[@]}"
+do
+    $TOOLPATH/prefetch -v $sra;
+done
+
+if [ ! -d "$OUTPATH" ]; then 
+    mkdir $OUTPATH;
+fi
+mv $HOME/ncbi/public/sra/*.sra $OUTPATH
+echo "Okay!"
